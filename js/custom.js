e00b78a99f24fa5e1bb5fab0310b6fb3836a441d
sgvaidyas <69918605+sgvaidyas@users.noreply.github.com>
2021-05-28 12:16:23 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+function showme()
+{
+    
+    var menubar = document.getElementById("menu");
+
+    if(menubar.style.display==="none"){
+        
+        menubar.style.display="block";
+    }
+    else{
+        menubar.style.display="none"
+    }
+}
